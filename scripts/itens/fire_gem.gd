fb5e8ed5e4e2e2455edeabf0205fbb09cd8973ef
Luiz fernando <86127864+Dranzule@users.noreply.github.com>
2026-06-23 12:36:13 -0300

    fix: hitbox das fire gems não sendo limpo

--- scripts/itens/fire_gem.gd
+++ scripts/itens/fire_gem.gd
@@ -6,8 +6,8 @@ extends Area2D
 # player will listen for changes on gem count
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		player.change_charge(true)
 		animation_player.play("pick_up")
+		player.change_charge(true)
 		
 		await animation_player.animation_finished
 		queue_free()
