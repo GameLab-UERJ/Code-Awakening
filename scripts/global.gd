b970cf3f95bb10f7f668ff898863e2773f9d0bcb
Luiz fernando <86127864+Dranzule@users.noreply.github.com>
2026-07-16 18:43:16 -0300

    fix: impedir repetição da contagem de objetivos

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,11 +3,13 @@ extends Node
 var last_world_positioin: Vector2 = Vector2.ZERO
 
 var health: float = 100
-var energy: float = 10
+var energy: float = 100
 var has_ever_died: bool = false
 
 var victories: int = 0
-var plaines_defeated: bool = false
-var montain_defeated: bool = false
+
 var block_cave_plaines: bool = false
+var plaines_counted: bool = false
+
 var block_cave_montains: bool = false
+var montains_counted: bool = false
--- scripts/world.gd
+++ scripts/world.gd
@@ -3,6 +3,7 @@ extends Node2D
 var current_scene: String = "world"
 
 @onready var player: CharacterBody2D = $Environment/Player
+@onready var transformer = get_tree().get_first_node_in_group("Transformer")
 @onready var hp_hud: Node2D = $Environment/hp_hud
 
 @onready var block_cave_plaines: TileMapLayer = $Environment/Caves/CavePlaines/BlockCavePlaines
@@ -21,25 +22,24 @@ func _ready() -> void:
 		player.health = Global.health
 		
 		hp_hud._on_energy_update(Global.energy)
-		player.energy = Global.energy
-		
-	if Global.block_cave_plaines and !Global.plaines_defeated:
-		if !block_cave_plaines.enabled:
-			Global.victories += 1
-			Global.plaines_defeated = true
-			block_cave_plaines.enabled = true	
-		
-		print("plain")
-
+		transformer.energy = Global.energy
 	
-	if Global.block_cave_montains and !Global.montain_defeated:
-		if !block_cave_montain.enabled:
+	if Global.block_cave_plaines:
+		block_cave_plaines.enabled = true
+		print("plaines conquered")
+		
+		if not Global.plaines_counted:
 			Global.victories += 1
-			Global.montain_defeated = true
-			block_cave_montain.enabled = true
-			
-		print("mount")
-
+			Global.plaines_counted = true
+	
+	if Global.block_cave_montains:
+		block_cave_montain.enabled = true
+		print("montain conquered")
+		
+		if not Global.montains_counted:
+			Global.victories += 1
+			Global.montains_counted = true
+		
 
 func _process(delta: float) -> void:
 	if Global.block_cave_montains and Global.block_cave_plaines:
@@ -61,7 +61,7 @@ func change_scene(change_to: String) -> void:
 	
 	Global.health = player.health
 	
-	Global.energy = player.energy
+	Global.energy = transformer.energy
 	
 	match change_to:
 		"montain":
